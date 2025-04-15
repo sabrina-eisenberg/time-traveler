493d000ed12d9352781e62b8f625a37ae46e442c
sabrina-eisenberg <sabrina_eisenberg@aol.com>
2025-04-15 02:33:24 -0400

    final

--- 2010s/script.js
+++ 2010s/script.js
@@ -1,4 +1,61 @@
-function toggleContent() {
-	let content = document.querySelector("#content");
+function toggleContent3() {
+	let content = document.querySelector("#Billie");
+	content.classList.toggle('hidden');
+}
+
+
+function toggleContent1() {
+	let content = document.querySelector("#Portugal");
+	content.classList.toggle('hidden');
+}
+
+
+function toggleContent2() {
+	let content = document.querySelector("#Carly");
+	content.classList.toggle('hidden');
+}
+
+function toggleContent4() {
+	let content = document.querySelector("#Harry");
+	content.classList.toggle('hidden');
+}
+
+function toggleContent5() {
+	let content = document.querySelector("#Lorde");
+	content.classList.toggle('hidden');
+}
+
+function toggleContent6() {
+	let content = document.querySelector("#Miley");
+	content.classList.toggle('hidden');
+}
+
+function toggleContent7() {
+	let content = document.querySelector("#Rhianna");
+	content.classList.toggle('hidden');
+}
+
+function toggleContent8() {
+	let content = document.querySelector("#Sia");
+	content.classList.toggle('hidden');
+}
+
+function toggleContent9() {
+	let content = document.querySelector("#fun");
+	content.classList.toggle('hidden');
+}
+
+function toggleContent10() {
+	let content = document.querySelector("#Taylor");
+	content.classList.toggle('hidden');
+}
+
+function toggleContent11() {
+	let content = document.querySelector("#Bruno");
+	content.classList.toggle('hidden');
+}
+
+function toggleContent12() {
+	let content = document.querySelector("#OneDirection");
 	content.classList.toggle('hidden');
 }
